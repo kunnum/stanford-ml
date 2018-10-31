d82f78c31360f852a67f36002f1ea99f00b56d67
kunnum <meeraj@servicesymphony.com>
2018-10-31 07:35:37 +0400

    One versus all training

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -9,10 +9,13 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 
 % Some useful variables
 m = size(X, 1);
+m
 n = size(X, 2);
+n
 
 % You need to return the following variables correctly 
 all_theta = zeros(num_labels, n + 1);
+size(all_theta)
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
@@ -50,15 +53,11 @@ X = [ones(m, 1) X];
 %
 
 
-
-
-
-
-
-
-
-
-
+for i = 1:num_labels,
+    initial_theta = zeros(n + 1, 1);
+    options = optimset('GradObj', 'on', 'MaxIter', 50);
+    all_theta(i, :) = fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), initial_theta, options);
+end
 
 % =========================================================================
 
