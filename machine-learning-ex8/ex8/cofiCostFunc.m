5fdd05e5662dfa86834e1b33cd5d7132e97a9c7c
kunnum <meeraj@servicesymphony.com>
2018-11-20 12:39:43 +0400

    Added CFI

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -1,5 +1,4 @@
-function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
-                                  num_features, lambda)
+function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda)
 %COFICOSTFUNC Collaborative filtering cost function
 %   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
 %   num_features, lambda) returns the cost and gradient for the
@@ -8,8 +7,7 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
 
 % Unfold the U and W matrices from params
 X = reshape(params(1:num_movies*num_features), num_movies, num_features);
-Theta = reshape(params(num_movies*num_features+1:end), ...
-                num_users, num_features);
+Theta = reshape(params(num_movies*num_features+1:end), num_users, num_features);
 
             
 % You need to return the following values correctly
@@ -39,20 +37,21 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
+J = 1 / 2 * sum(sum(R .* (X * Theta' - Y).^2)) + lambda / 2 * (sum(sum(Theta .^ 2)) + sum(sum(X .^ 2)));
 
+for i=1:num_movies
+    idx = find(R(i, :) == 1);
+    tempTheta = Theta(idx, :);
+    tempY = Y(i, idx);
+    X_grad(i, :) = (X(i, :) * tempTheta' - tempY) * tempTheta + lambda * X(i, :);
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
+for i=1:num_users
+    idx = find(R(:, i) == 1);
+    tempX = X(idx, :);
+    tempY = Y(idx, i);
+    Theta_grad(i, :) = (tempX * Theta(i, :)' - tempY)' * tempX + lambda * Theta(i, :);
+end
 
 
 % =============================================================
